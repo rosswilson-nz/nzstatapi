7000f21fb8828a3d54128a2ecb05bd48ac0ab345
Ross Wilson <ross.wilson@otago.ac.nz>
2025-11-05 20:01:56 +1300

    add `set_api_key()` helper function

--- R/utils.R
+++ R/utils.R
@@ -5,6 +5,25 @@ get_base_url <- function() {
   )
 }
 
+#' Store Aotearoa Data Explorer API key in environment variable
+#'
+#' A helper function to set the `NZSTAT_API_KEY` env var.
+#'
+#' @param key String: API key. If not specified, [askpass::askpass()] will
+#'     be used to provide a prompt to enter the key.
+#'
+#' @export
+set_api_key <- function(key = NULL) {
+  if (is.null(key)) {
+    key <- askpass::askpass("Please enter your API subscription key")
+  }
+  Sys.setenv("NZSTAT_API_KEY" = key)
+  cli::cli_inform(c(
+    v = "Key saved to env var {.var NZSTAT_API_KEY}",
+    i = "To store this permanently, add the key to your {.file .Renviron} file"
+  ))
+}
+
 get_api_key <- function() {
   key <- Sys.getenv("NZSTAT_API_KEY")
   if (identical(key, "")) {
